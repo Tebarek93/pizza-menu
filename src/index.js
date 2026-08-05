ccf0bdd47a9a3845ae65ee65d61218a8525c5d44
Tebarek Abas <tebarekabas1993@gmail.com>
2026-08-05 01:45:08 -0700

    add pros in the componte

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,56 +49,74 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
-      <Pizza />
       <Menu />
+    
       <Footer />
     </div>
   );
 }
 
-function Pizza() {
-  return (
-    <div>
-      <img src="/pizzas/focaccia.jpg" alt="Focaccia" />
-      <h3>Focaccia</h3>
-      <p>Bread with italian olive oil and rosemary</p>
-      <span>$6</span>
 
-      <img src="/pizzas/margherita.jpg" alt="Pizza Margherita" />
-      <h3>Pizza Margherita</h3>
-      <p>Tomato and mozarella</p>
-      <span>$10</span>
-    </div>
-  );
-}
+
+
+
 
 function Header() {
   return (
-    <header className="header">
-      <h1>Fast React Pizza Co.</h1>
+    <header className="header footer">
+      <h1 className="header" >Fast React Pizza Co.</h1>
     </header>
   );
 }
+
+
+
+
+
 
 function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+      <Pizza img="/pizzas/focaccia.jpg" name="Focaccia" ingredients="Bread with italian olive oil and rosemary" price={6} />
+      <Pizza img="/pizzas/margherita.jpg" name="Pizza Margherita" ingredients="Tomato and mozarella" price={10} />
+      <Pizza img="/pizzas/spinaci.jpg" name="Pizza Spinaci" ingredients="Tomato, mozarella, spinach, and ricotta cheese" price={12} />
+      <Pizza img="/pizzas/funghi.jpg" name="Pizza Funghi" ingredients="Tomato, mozarella, mushrooms, and onion" price={12} />
+      <Pizza img="/pizzas/salamino.jpg" name="Pizza Salamino" ingredients="Tomato, mozarella, and pepperoni" price={15} />
+      <Pizza img="/pizzas/prosciutto.jpg" name="Pizza Prosciutto" ingredients="Tomato, mozarella, ham, aragula, and burrata cheese" price={18} />
     </main>
   );
 }
 
+function Pizza({ img, name, ingredients, price }) {
+  return (
+    <div className="pizza">
+      <img src={img} alt={name} />
+      <h3>{name}</h3>
+      <div className="ingredients">
+        <p>{ingredients}</p>
+      <span>${price.toFixed(2)}</span>
+      </div>
+
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
 function Footer() {
   const hour = new Date().getHours();
-  console.log(hour)
   const openHour = 4;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-console.log(isOpen);
-if(hour >= openHour && hour <= closeHour) alert("We are currently open!");
-else alert("We are currently closed!");
+
  
   return (
     <footer className="footer">
